79434d2a52c42a7cbf3a243089af3b6a06b364a4
Unknown <bastien_matthai1@hotmail.fr>
2018-05-25 23:03:23 +0200

    adding TestLvl

--- level_generation.c
+++ level_generation.c
@@ -6,17 +6,36 @@
 
 //Generetate level
 t_level generate_level_randomly(int width, int height) {
-    
+
 }
 
 //Generate a level from a file
 t_level generate_level_from_file(char *file) {
-    
+
+  int fd;
+  t_level level;
+
+  char lines;
+  char columns;
+
+  *level.terrain = malloc(lines * sizeof(char*));
+  fd = open("./level/testlevel.lvl", O_RDONLY);
+
+  lines = 10;
+  columns = 14;
+
+  for (int i = 0; i < lines; i++) {
+    *level.terrain[i] = malloc(sizeof(char) * columns);
+    read(fd, *level.terrain[i], count);
+    lseek(fd, 1, SEEK_CUR);
+  }
+  return level;
 }
 
 int free_level(t_level *level) {
-    
+
 }
 
-
-
+char *read_file_line(int fd) {
+  read();
+}
