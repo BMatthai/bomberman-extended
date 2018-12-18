78070c01cdaf4d8335be81a17fccdab97cea31b3
BMatthai <bastien_matthai1@hotmail.fr>
2018-12-18 01:05:14 +0100

    Maze_2

--- maze_generation.c
+++ maze_generation.c
@@ -54,6 +54,49 @@ int count_characters(t_level *level) {
   return number_characters;
 }
 
+typedef struct s_set {
+    int index;
+    struct s_set *next;
+} t_set;
+
+
+int is_same_set(t_set *set1, t_set *set2) {
+  t_set *cur_elt1 = set1;
+  t_set *cur_elt2 = set2;
+
+  int value_elt1 = cur_elt1->value;
+  int value_elt2 = cur_elt2->value;
+
+  while((cur_elt1 = cur_elt1->next) != NULL)) {
+    if (cur_elt1->value = value_elt2) {
+      return YES;
+    }
+  }
+
+  while((cur_elt2 = cur_elt2->next) != NULL)) {
+    if (cur_elt2->value = value_elt1) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+void merge_sets(t_set *set1, t_set *set2) {
+  t_set *cur_elt = NULL;
+
+  cur_elt = set1;
+  while((cur_elt = cur_elt->next) != NULL);
+  cur_elt->next = set2;
+}
+
+int count_walls(int height, int width) {
+  return (height * width) / 2;
+}
+
+int count_cells(int height, int width) {
+  return (height * width) / 4;
+}
+
 void fill_array_with_wall(char **maze, int height, int width) {
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
@@ -62,31 +105,49 @@ void fill_array_with_wall(char **maze, int height, int width) {
   }
 }
 
-// void shuffle() {
-//
-// }
+void shuffle(int *walls, int height, int width) {
+  int size = count_walls(height, width);
+
+  if (size > 1) {
+      int i;
+      for (i = 0; i < size - 1; i++) {
+        int j = i + rand() / (RAND_MAX / (size - i) + 1);
+        int t = walls[j];
+        walls[j] = walls[i];
+        walls[i] = t;
+      }
+  }
+}
 
 int *list_walls(int height, int width) {
-  int size_needed = (height * width) / 2;
-  int *walls = malloc(sizeof(int) * size_needed);
+  int size = count_walls(height, width);
+  int *walls = malloc(sizeof(int) * size);
 
-  for (int i = 0; i < size_needed; i++) {
+  for (int i = 0; i < size; i++) {
     if (((i * 2 / width) % 2) == 0) {
       walls[i] = i * 2;
     }
     else {
       walls[i] = i * 2 + 1;
     }
-    printf("%d\n", walls[i]);
   }
   return walls;
 }
 
-// Create a list of all walls, and create a set for each cell, each containing just that one cell.
-// For each wall, in some random order:
-// If the cells divided by this wall belong to distinct sets:
-// Remove the current wall.
-// Join the sets of the formerly divided cells.
+t_set *create_sets(int height, int width) {
+  int size = count_cells(height, width);
+
+  t_set *set = NULL;
+  set = malloc(size * sizeof(t_set));
+
+  int ligne;
+  for (int i = 0; i < size; i++) {
+    ligne = i / width;
+    set[i]->index = (2 * ligne) + (2 * i + 1);
+    set[i]->next = NULL;
+  }
+}
+
 
 char **generate_empty_layer(int height, int width) {
   char **layer;
@@ -98,14 +159,58 @@ char **generate_empty_layer(int height, int width) {
   return layer;
 }
 
+void remove_wall(char **maze, int height, int width, int index) {
+  int i;
+  int j;
+
+  i = index / width;
+  j = index % width;
+
+  maze[j][i] = ' ';
+}
+
+t_set *set_from_index(t_set *sets, int index) {
+
+}
+
+void creuser(char **maze, int *wall_indexes, int height, int width) {
+  int size = count_walls(height, width);
+
+  t_set *sets = create_sets(height, width);
+
+  int is_even;
+  for (int i = 0; i < size; i++) {
+
+    is_even = ((i / width) % 2) == 1 ? YES : NO;
+
+    t_set *set1 = NULL;
+    t_set *set2 = NULL;
+
+    if (is_even == YES) {
+      set1 = set_from_index(sets, i - 1);
+      set2 = set_from_index(sets, i + 1);
+    }
+    else {
+      set1 = set_from_index(sets, i - width);
+      set2 = set_from_index(sets, i + width);
+    }
+
+    if (the cells divided by this wall belong to distinct sets) {
+      remove_wall(maze, height, width, i);
+      merge_sets(set1, set2);
+    }
+  }
+}
+
 char **generate_maze(int height, int width) {
   char **maze;
 
   maze = generate_empty_layer(height, width);
   fill_array_with_wall(maze, height, width);
 
-  int *index = list_walls(height, width);
-
+  int *wall = list_walls(height, width);
+  shuffle(wall, height, width);
+  creuser();
   return maze;
 }
 
